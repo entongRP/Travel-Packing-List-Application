782c7e518522cae2576071fc16fa5d5b5c942674
entongRP <24012306@myrp.edu.sg>
2025-11-25 10:50:16 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,80 @@
+import { useState } from "react";
+
+// Initial packing items
+const initialItems = [
+  { id: 1, description: "Shirt", quantity: 5, packed: true },
+  { id: 2, description: "Pants", quantity: 2, packed: false },
+];
+
+function Logo() {
+  return <h1>My Travel List</h1>;
+}
+
+function Form() {
+
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need to pack?</h3>
+
+     {/* drop down list */}
+      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
+        <option value={1}>1</option>
+        <option value={2}>2</option>
+        <option value={3}>3</option>
+      </select>
+
+    {/* input field */}
+      <input type="text" placeholder="Item..." value={description} onChange={(e) => setDescription(e.target.value)}/>
+      
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item key={item.id} item={item}/>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item ({item}) {
+  return (
+    <li style={{ textDecoration: item.packed ? "line-through" : "none" }}>
+    {item.quantity} - {item.description}
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items in the list. You already packed Y (Z%).</em>
+    </footer>
+  );
+}
+
+function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+export default App;
